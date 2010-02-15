d74eee6cb610537e38eb7e9c1e48524222889e45
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-02-15 10:37:33 +0100

    initial import from BigBand

---
+++ README.md
@@ -0,0 +1,44 @@
+Sinatra::Compass
+================
+
+Integrates the Compass stylesheet framework with [Sinatra](http://sinatrarb.com).
+
+BigBand
+-------
+
+Sinatra::Compass is part of the [BigBand](http://github.com/rkh/big_band) stack.
+Check it out if you are looking for other fancy Sinatra extensions.
+
+Usage
+-----
+
+Usage without doing something:
+
+  require "sinatra"
+  require "sinatra/compass"
+
+If you create a directory called views/stylesheets and place your
+sass files in there, there you go. Just call stylesheet(name) form
+your view to get the correct stylesheet tag. The URL for your
+stylesheets will be /stylesheets/:name.css.
+
+Of course you can use any other setup. Say, you want to store your
+stylesheets in views/css and want the URL to be /css/:name.css:
+
+  get_compass("css")
+
+But what about more complex setups?
+
+  require "sinatra/base"
+  require "sinatra/compass"
+
+  class Foo < Sinatra::Base
+    register Sinatra::Compass
+    set :compass, :sass_dir => "/foo/bar/blah"
+    get_compass("/foo/:name.css") do
+      compass :one_stylesheet
+    end
+  end
+
+Note that already generated routes will be deactivated by calling
+get_compass again.
---
+++ lib/sinatra/compass.rb
@@ -0,0 +1,64 @@
+require "sinatra/base"
+require "sinatra/sugar"
+require "sinatra/advanced_routes"
+
+module Sinatra
+  module Compass
+
+    module ClassMethods
+      attr_reader :compass_route
+      def get_compass(path, &block)
+        block ||= Proc.new do |file|
+          content_type 'text/css', :charset => 'utf-8'
+          compass :"#{path}/#{params[:name]}"
+        end
+        set :compass, :sass_dir => views / path unless compass[:sass_dir] && compass[:sass_dir].directory?
+        @compass_route.deactivate if @compass_route
+        @compass_route = get("/#{path}" / ":name.css", &block)
+      end
+    end
+
+    module InstanceMethods
+      def compass(file, options = {})
+        options.merge! ::Compass.sass_engine_options
+        sass file, options
+      end
+      def stylesheet(*args)
+        raise NotImplementedError, "yet to be implemented"
+      end
+    end
+
+    def self.registered(klass)
+      klass.register Sugar
+      klass.register AdvancedRoutes
+      klass.extend ClassMethods
+      klass.send :include, InstanceMethods
+      klass.set :compass,
+      :project_path => klass.root_path, :output_style => (klass.development? ? :expanded : :compressed),
+      :sass_dir => klass.views / "stylesheets", :line_comments => klass.development?
+      set_app_file(klass) if klass.app_file?
+    end
+
+    def self.set_app_file(klass)
+      klass.set :compass, :root_path => klass.root_path
+      klass.get_compass("stylesheets") if (klass.views / "stylesheets").directory?
+    end
+
+    def self.set_compass(klass)
+      ::Compass.configuration do |config|
+        config.sass_options ||= {}
+        klass.compass.each do |option, value|
+          if config.respond_to? option
+            config.send "#{option}=", value
+          else
+            config.sass_options.merge! option.to_sym => value
+          end
+        end
+      end
+    end
+
+  end
+
+  register Compass
+
+end
